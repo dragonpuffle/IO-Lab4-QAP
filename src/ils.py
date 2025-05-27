7c8a35ceee0f6e2c1ad415d1442b8462e2a38b8e
Podvigin Andrew <109742476+dragonpuffle@users.noreply.github.com>
2025-05-28 01:03:25 +0300

    Algorithms structure

---
+++ src/ils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from src.local_search import LocalSearch
+
+
+class IteratedLocalSearch(LocalSearch):
+    def __init__(self):
+        ...
+
+    def petrubation(self):
+        ...
+
+    def iterated_solve(self):
+        """Local search - first-improvement + don't look bits"""
+        ...
+
+
+if __name__ == "__main__":
+    ...
---
+++ src/local_search.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class LocalSearch:
+    def __init__(self):
+        ...
+
+    def total_cost(self):
+        ...
+
+    def delta_cost(self):
+        ...
+
+    def solve(self):
+        """Local search - first-improvement + don't look bits"""
+        ...
+
+
+def parse_data():
+    ...
+
+
+if __name__ == "__main__":
+    ...
